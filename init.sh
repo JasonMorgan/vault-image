70ac6714413c2a5df814f9ec9c83eb3d4c3c813e
Jason Morgan <jmorgan@f9vs.com>
2019-01-08 22:45:36 -0700

    changing the path.

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat > /etc/vault/config/vault.hcl <<EOF
+cat > /vault/config/vault.hcl <<EOF
 listener "tcp" {
   address = "0.0.0.0:8200"
 }
@@ -10,4 +10,4 @@ storage "s3" {
 ui = true
 EOF
 
-vault server -config /etc/vault/config/vault.hcl
+vault server -config /vault/config/vault.hcl

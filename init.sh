b00c55e997460dc8e86c0da7ef1639d3d88b0d74
Jason Morgan <jmorgan@f9vs.com>
2019-01-22 17:19:39 -0500

    changing to a local store.

--- init.sh
+++ init.sh
@@ -4,12 +4,12 @@ listener "tcp" {
   address = "0.0.0.0:8200"
 }
 
-storage "s3" {
+storage "file" {
+  path = "/vault/data"
 }
 
 ui = true
 EOF
 
 cat /vault/config/vault.hcl
-env
 vault server -config /vault/config/vault.hcl

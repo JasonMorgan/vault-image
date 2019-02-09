2317be26bcb1d8d170eda0aa1ca773f8318ba039
Jason Morgan <jmorgan@f9vs.com>
2019-02-09 17:50:50 -0500

    Fixing the bucket resource.
    It doesn't work with env vars in spite of what the docs say.

--- init.sh
+++ init.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 cat > /vault/config/vault.hcl <<EOF
+
 listener "tcp" {
   address = "0.0.0.0:8200"
+  tls_disable = true
 }
 
-storage "file" {
-  path = "/vault/data"
-}
+# storage "file" {
+#   path = "/vault/data"
+# }
 
-diable_mlock = true
+storage "s3" {
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
+  bucket     = "$AWS_S3_BUCKET"
+}
 
 ui = true
 
 EOF
 
-cat /vault/config/vault.hcl
 vault server -config /vault/config/vault.hcl
